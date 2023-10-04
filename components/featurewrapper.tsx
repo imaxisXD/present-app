8c1b00010b66c74d527f498d61b1e6473e4ec347
imaxisXD <sunny735084@gmail.com>
2023-10-04 17:43:31 +0530

    disabling: feat card

--- components/featurewrapper.tsx
+++ components/featurewrapper.tsx
@@ -3,7 +3,7 @@ function FeatureWrapper() {
     return (
         <div className="flex flex-col items-center w-full p-4">
             <FeatureCard title="Step 1: Search" img="/search-card1.webp" zIndex={50} />
-            <div className="sticky bottom-[9vh] z-40 w-11/12 h-[600px] bg-green-300 rounded-lg border border-[#555] m-2 ">
+            {/* <div className="sticky bottom-[9vh] z-40 w-11/12 h-[600px] bg-green-300 rounded-lg border border-[#555] m-2 ">
                 <h1>Step 1: Search</h1>
                 <div className="text-center">
                     <p>Search influencer from any social media platform</p>
@@ -14,7 +14,7 @@ function FeatureWrapper() {
                 <div className="text-center">
                     <p>Search influencer from any social media platform</p>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
