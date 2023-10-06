c9e5dcb78fbebf65dd47f1f211bc05ff8ba886b8
imaxisXD <sunny735084@gmail.com>
2023-10-06 14:23:50 +0530

    opengraph added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,35 @@ import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react';
 import dynamic from 'next/dynamic'
 
+const baseUrl = process.env.NEXT_PUBLIC_ROOT_DOMAIN
+  ? `https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
+  : 'http://localhost:3000';
+
 export const metadata: Metadata = {
   title: 'Present - Automate Influencer marketing',
   description: 'Present is a all in one tool that you need to automate the Influencer marketing campaigns for you. Define Budget and select influencers from any social media and let us handle the rest for you',
+  openGraph: {
+    title: 'Present - Automate Influencer marketing',
+    description: 'Present is a all in one tool that you need to automate the Influencer marketing campaigns for you. Define Budget and select influencers from any social media and let us handle the rest for you',
+    images: [
+      {
+        url: 'https://www.trypresentapp.com/_static/opengraph.png',
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: 'Present - Automate Influencer marketing',
+    description: 'Present is a all in one tool that you need to automate the Influencer marketing campaigns for you. Define Budget and select influencers from any social media and let us handle the rest for you',
+    images: ['https://www.trypresentapp.com/_static/opengraph.png'],
+    creator: "@abhishk_084",
+  },
+  metadataBase: new URL(baseUrl),
+  robots: {
+    follow: true,
+    index: true
+  },
+
 }
 const CrispWithNoSSR = dynamic(
   () => import('../components/crisp')
